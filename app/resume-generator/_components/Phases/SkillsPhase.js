7df8d3b78f483586f7a82cede62077d62b42e598
Saurabh Phawade <saurvphawade09@gmail.com>
2024-12-17 16:45:48 +0530

    Updated SkillsPhase component with new features and functionality

--- app/resume-generator/_components/Phases/SkillsPhase.js
+++ app/resume-generator/_components/Phases/SkillsPhase.js
@@ -2,22 +2,53 @@
 
 import React, { useState } from "react";
 import TitleComponent from "../TitleComponent";
+import SkillList from "../SkillList";
+import { LuSearch } from "react-icons/lu";
 
 export default function SkillsPhase() {
   const [skillsData, setSkillsData] = useState({
-    jobTitle: "",
-    skills: [],
+    jobTitle: "Full Stack Developer",
+    skills: ["Python", "Ruby"],
   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setSkillsData({
-      ...skillsData,
-      [name]: value,
-    });
-  };
+  const [relatedSkills, setRelatedSkills] = useState([
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React JS",
+    "Next JS",
+    "Node JS",
+    "REST APIs",
+    "Angular",
+    "Redux",
+    "Bootstrap",
+    "Tailwind CSS",
+    "Git",
+  ]);
 
   console.log("skillsData", skillsData);
+
+  // Function to Add skill to my skills and Remove skill from related skills
+  const addToSkills = (skill) => {
+    setSkillsData({
+      ...skillsData,
+      skills: [...skillsData.skills, skill],
+    });
+
+    setRelatedSkills((prevSkills) =>
+      prevSkills.filter((cval) => cval !== skill)
+    );
+  };
+
+  // Function to Remove skill from my skills and Add skill to related skills
+  const removeFromSkills = (skill) => {
+    setSkillsData({
+      ...skillsData,
+      skills: skillsData.skills.filter((cval) => cval !== skill),
+    });
+
+    setRelatedSkills((prevSkills) => [...prevSkills, skill]);
+  };
 
   return (
     <div>
@@ -39,7 +70,9 @@ export default function SkillsPhase() {
             id={`jobTitle`}
             className="w-full px-2 py-1 border border-[#808080] rounded focus:outline-none text-sm font-normal"
             value={skillsData.jobTitle}
-            onChange={handleChange}
+            onChange={(e) =>
+              setSkillsData({ ...skillsData, jobTitle: e.target.value })
+            }
           />
         </div>
         <div>&nbsp;</div>
@@ -48,23 +81,29 @@ export default function SkillsPhase() {
           <label className="block text-[#252525] text-base font-medium mb-1">
             Skills in Your Resume *
           </label>
-          <div className="w-full px-2 py-1 border border-[#808080] rounded text-sm font-normal">
-            <p>Skill 1</p>
-            <p>Skill 2</p>
-          </div>
+          <SkillList
+            skills={skillsData?.skills}
+            onAction={removeFromSkills}
+            actionType="remove"
+          />
         </div>
         <div>
           <label className="block text-[#252525] text-base font-medium mb-1">
             Showing examples *
           </label>
-          <input
-            type="search"
-            className="w-full px-2 py-1 border border-[#808080] rounded focus:outline-none text-sm font-normal"
-          />
-          <div className="w-full px-2 py-1 border border-[#808080] rounded text-sm font-normal my-3">
-            <p>Skill 1</p>
-            <p>Skill 2</p>
+          <div className="relative">
+            <input
+              type="search"
+              placeholder="Search"
+              className="w-full ps-8 pe-2 py-1 border border-[#808080] rounded-md focus:outline-none text-sm font-normal"
+            />
+            <LuSearch className="absolute top-[0.3rem] left-[0.3rem] text-xl text-[#808080]" />
           </div>
+          <SkillList
+            skills={relatedSkills}
+            onAction={addToSkills}
+            actionType="add"
+          />
         </div>
         <hr className="my-4 border border-[#C7C7C7] col-span-2" />
       </div>
---
+++ app/resume-generator/_components/SkillList.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { FaPlus } from "react-icons/fa6";
+import { FaMinus } from "react-icons/fa6";
+
+export default function SkillList({
+  skills,
+  onAction,
+  actionType = "add", // 'add' or 'remove'
+}) {
+  return (
+    <div className="w-full min-h-[340px] p-2 border border-[#808080] rounded text-sm font-normal my-3 flex flex-wrap content-start gap-3">
+      {skills.map((cval) => (
+        <div
+          key={cval}
+          className="border border-[#DBDBDE] shadow-sm rounded-md text-nowrap px-3 py-1 h-fit text-sm text-[#414141] font-normal"
+        >
+          {actionType == "add" ? (
+            <FaPlus
+              onClick={() => onAction(cval)}
+              className="inline cursor-pointer me-2"
+            />
+          ) : (
+            <FaMinus
+              onClick={() => onAction(cval)}
+              className="inline cursor-pointer me-2"
+            />
+          )}
+          {cval}
+        </div>
+      ))}
+    </div>
+  );
+}
